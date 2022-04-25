042953b95b52b8ce30f7ac2ee859ce9b7b45a9a2
adrianus007 <87032107+adrianus007@users.noreply.github.com>
2022-04-25 15:05:49 +0700

    Update..

---
+++ app/src/main/java/com/example/tr_pamb/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.tr_pamb.Prevalent;
+
+import com.example.tr_pamb.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
