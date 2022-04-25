cc8d395254c589fbe3478c0765f6fd89a52fe2cc
Dendy <dendyi44@gmail.com>
2022-04-26 02:50:11 +0700

    Menambahkan activity cart

---
+++ app/src/main/java/com/example/tr_pamb/CartActivity.java
@@ -0,0 +1,33 @@
+package com.example.tr_pamb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CartActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private Button NextProcessBtn;
+    private TextView txtTotalAmount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+
+        recyclerView = findViewById(R.id.cart_list);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        NextProcessBtn = findViewById(R.id.next_process_btn);
+        txtTotalAmount = findViewById(R.id.total_price);
+
+    }
+}
