c99f17590f2c3b08a82d3b0e103d0e42c76b00c4
Dendy <dendyi44@gmail.com>
2022-04-26 05:33:34 +0700

    Menambahkan Confirm order

--- app/src/main/java/com/example/tr_pamb/CartActivity.java
+++ app/src/main/java/com/example/tr_pamb/CartActivity.java
@@ -67,6 +67,9 @@ public class CartActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
+
+
+
         final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart List");
 
         FirebaseRecyclerOptions<Cart> options =
